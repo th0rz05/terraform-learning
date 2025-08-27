13546157ad7bee5305e4537426cd4d2d291b7748
Tiago Barbosa <tiago.filipe.barbosa@gmail.com>
2025-08-27 15:58:30 +0100

    Comment out AWS instance resource and retain VPC and subnet configurations

--- project1/main.tf
+++ project1/main.tf
@@ -4,10 +4,27 @@ provider "aws" {
   secret_key = var.aws_secret_key
 }
 
-resource "aws_instance" "my-first-server"{
-    ami = "ami-0360c520857e3138f"
-    instance_type = "t3.micro"
-    tags = {
-      Name = "MyFirstServer"
-    }
+# resource "aws_instance" "my-first-server"{
+#     ami = "ami-0360c520857e3138f"
+#     instance_type = "t3.micro"
+#     tags = {
+#       Name = "MyFirstServer"
+#     }
+# }
+
+resource "aws_vpc" "first-vpc" {
+  cidr_block = "10.0.0.0/16"
+  
+  tags = {
+    Name = "FirstVPC"
+  }
+}
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id     = aws_vpc.first-vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "FirstSubnet"
+  }
 }
